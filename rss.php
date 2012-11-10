5f3d1272a715de7361dee23dc22d16024bb067d7
Gemorroj <wapinet@gmail.com>
2012-11-11 00:01:42 +0300

    для генерации rss использован класс

--- rss.php
+++ rss.php
@@ -34,48 +34,15 @@
  */
 
 
-header('Content-Type: application/rss+xml; charset=UTF-8');
-
 require 'moduls/config.php';
+require 'moduls/inc/Rss.php';
+
 define('DIRECTORY', str_replace(array('\\', '//'), '/', dirname($_SERVER['PHP_SELF']) . '/'));
 
 $link = 'http://' . $_SERVER['HTTP_HOST'] . DIRECTORY . 'news.php';
 
-class rss2 extends DOMDocument
-{
-    private $_channel;
 
-
-    public function __construct($title, $link, $description)
-    {
-        parent::__construct('1.0', 'UTF-8');
-        //$this->formatOutput = true;
-
-        $root = $this->createElement('rss');
-        $root->setAttribute('version', '0.92');
-        $rss = $this->appendChild($root);
-
-        $this->_channel = $rss->appendChild($this->createElement('channel'));
-
-        $this->_channel->appendChild($this->createElement('title', $title));
-        $this->_channel->appendChild($this->createElement('link', $link));
-        $this->_channel->appendChild($this->createElement('description', $description));
-    }
-
-
-    public function addItem($title, $link, $description)
-    {
-        $item = $this->createElement('item');
-        $item->appendChild($this->createElement('title', $title));
-        $item->appendChild($this->createElement('link', $link));
-        $item->appendChild($this->createElement('description', $description));
-        $this->_channel->appendChild($item);
-    }
-}
-
-
-$rss = new rss2($language['news'], $link, $language['news']);
-
+$rss = new Rss($language['news'], $link, $language['news']);
 
 $q = mysql_query(
     '
@@ -90,11 +57,14 @@ $q = mysql_query(
 
 
 while ($arr = mysql_fetch_assoc($q)) {
+    $date = new DateTime('@' . $arr['time']);
+
     $rss->addItem(
-        'Новость - ' . date('Y.m.d H:i', $arr['time']),
+        'Новости - ' . $date->format('Y.m.d H:i'),
         $link,
-        '<div>' . $arr['news'] . '</div>'
+        '<div>' . $arr['news'] . '</div>',
+        $date
     );
 }
 
-echo trim($rss->saveXML());
+$rss->output();
