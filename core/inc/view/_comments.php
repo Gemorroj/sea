494dd855fc8d6479853971098779d5cdbc2fd6dc
Gemorroj <wapinet@gmail.com>
2013-11-21 18:37:10 +0300

    Исправлены комментарии в описании файла

--- core/inc/view/_comments.php
+++ core/inc/view/_comments.php
@@ -7,7 +7,7 @@ if (Config::get('comments_view') && $commentsCount) {
 
     $q = $db->prepare('SELECT `name`, `text`, `time` FROM `comments` WHERE `file_id` = ? ORDER BY `id` DESC LIMIT ?');
     $q->bindValue(1, $id, PDO::PARAM_INT);
-    $q->bindVAlue(2, Config::get('comments_view'), PDO::PARAM_INT);
+    $q->bindValue(2, intval(Config::get('comments_view')), PDO::PARAM_INT);
     $q->execute();
 
     $comments = $q->fetchAll();
